2f6156b7640b4fc24e8ecaada13908e23934cd65
VIK2395 <vik.chupak@gmail.com>
2021-04-01 00:08:19 +0300

    Base data-driven table done

--- client/main.js
+++ client/main.js
@@ -1,9 +1,76 @@
-const getDataBtn = document.getElementById('getDataBtn');
+class Grid {
+  constructor(columnsConfig, tableData, tableCaption) {
+    // columns: [{field: .., title: ..}]
+    // data = [{}, {} ...]
+    this.columns = columnsConfig;
+    this.data = tableData;
+    this.caption = tableCaption;
+  }
 
-async function getData() {
-  const response = await fetch('http://localhost:5000/');
-  const grid = await response.json();
-  console.log('fetched grid: ', grid);
+  render(rootId) {
+    const root = document.getElementById(rootId);
+
+    let headers = '';
+
+    for (const column of this.columns) {
+      headers += `<th>${column.title ? column.title : column.field}</th>`;
+    }
+
+    const tableHeaderRow = `<tr>${headers}</tr>`;
+
+    let tableBodyRows = '';
+
+    for (const row of this.data) {
+      let cells = '';
+      for (const column of this.columns) {
+        cells += `<td>${row[column.field]}</td>`;
+      }
+      const tableBodyRow = `<tr>${cells}</tr>`;
+      tableBodyRows += tableBodyRow;
+    }
+
+    const caption = this.caption ? `<caption>${this.caption}</caption>` : '';
+
+    const table = `<table>
+          ${caption}
+          <thead>
+            ${tableHeaderRow}
+          </thead>
+          <tbody>
+            ${tableBodyRows}
+          </tbody>
+        </table>`;
+
+    root.innerHTML = table;
+  }
 }
 
-getDataBtn.addEventListener('click', getData);
+const createTableBtn = document.getElementById('createTableBtn');
+
+async function fetchData() {
+  const response = await fetch('http://localhost:5000/');
+  const data = await response.json();
+  return data;
+}
+
+const columns = [
+  {
+    field: 'name',
+    title: 'Person',
+  },
+  {
+    field: 'job',
+    title: 'Job',
+  },
+  {
+    field: 'country',
+    title: 'Country',
+  },
+];
+
+async function createTable() {
+  const data = await fetchData();
+  new Grid(columns, data, 'Table Caption').render('table');
+}
+
+createTableBtn.addEventListener('click', createTable);

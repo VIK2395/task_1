eef2c91a49c1a517645be9841f251f026041c39d
VIK2395 <vik.chupak@gmail.com>
2021-04-01 03:48:04 +0300

    Sorting added to the table

--- client/main.js
+++ client/main.js
@@ -1,10 +1,39 @@
 class Grid {
-  constructor(columnsConfig, tableData, tableCaption) {
+  constructor(columnsConfig, tableData, tableCaption = undefined) {
     // columns: [{field: .., title: ..}]
     // data = [{}, {} ...]
     this.columns = columnsConfig;
     this.data = tableData;
     this.caption = tableCaption;
+  }
+
+  sortData(byField) {
+    function compare(field, order = 'asc') {
+      return (a, b) => {
+        if (!a.hasOwnProperty(field) || !b.hasOwnProperty(field)) {
+          return 0;
+        }
+
+        const A = typeof a[field] === 'string' ? a[field].toLowerCase() : a[field];
+        const B = typeof b[field] === 'string' ? b[field].toLowerCase() : b[field];
+
+        if (order === 'asc') {
+          if (A < B) return -1;
+          if (A > B) return 1;
+        }
+
+        if (order === 'desc') {
+          if (A > B) return -1;
+          if (A < B) return 1;
+        }
+
+        return 0;
+      };
+    }
+
+    this.data.sort(compare(byField));
+
+    return this;
   }
 
   render(rootId) {
@@ -13,7 +42,7 @@ class Grid {
     let headers = '';
 
     for (const column of this.columns) {
-      headers += `<th>${column.title ? column.title : column.field}</th>`;
+      headers += `<th id=${column.field}>${column.title ? column.title : column.field}</th>`;
     }
 
     const tableHeaderRow = `<tr>${headers}</tr>`;
@@ -42,6 +71,14 @@ class Grid {
         </table>`;
 
     root.innerHTML = table;
+
+    const bindedSortData = this.sortData.bind(this);
+
+    root.addEventListener('click', function sortListener(ev) {
+      if (ev.target.tagName !== 'TH') return;
+      root.removeEventListener('click', sortListener);
+      bindedSortData(ev.target.id).render(rootId);
+    });
   }
 }
 
@@ -70,7 +107,7 @@ const columns = [
 
 async function createTable() {
   const data = await fetchData();
-  new Grid(columns, data, 'Table Caption').render('table');
+  new Grid(columns, data, 'Table Caption').render('root');
 }
 
 createTableBtn.addEventListener('click', createTable);
